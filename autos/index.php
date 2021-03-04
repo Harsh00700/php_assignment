1b309e7e4158679f0b800e78214f53f2386787a0
Harsh Gandhi <harshagandhi90095@gmail.com>
2021-03-05 02:40:46 +0530

    autos

---
+++ autos/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>HARSH</title>
+		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	</head>
+	<body style="font-family: josefin sans">
+		<div class="container">
+			
+			<h1>
+			Welcome To The Autos Database
+			</h1>
+			<p>
+				<a href="login.php">Please Log In !</a>
+			</p>
+			<p>
+				Attempt to go to the <a href="view.php">view.php</a> without logging in - it should fail with an error message.
+			</p>
+			<p>
+				Attempt to go to the <a href="add.php">add.php</a> without logging in - it should fail with an error message.
+			</p>
+		</div>
+	</body>
+</html>
