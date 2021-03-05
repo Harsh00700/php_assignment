bfdcef28a23a55cb82dea00c178f35fa488ac24d
Harsh Gandhi <harshagandhi90095@gmail.com>
2021-03-05 18:29:38 +0530

    autos

--- autos/view.php
+++ autos/view.php
@@ -36,7 +36,7 @@
 		<title>HARSH</title>
 		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 	</head>
-	<body style="font-family: josefin sans;">
+	<body style="font-family: josefin sans">
 		<div class="container">
 			<h1>
 			Tracking Autos For <?php echo($name); ?>
